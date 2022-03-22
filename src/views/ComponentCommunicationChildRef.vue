5e59b266e90ae7dd5af56c0e44d9b1b646f571b5
申思维 <shensiwei@sina.com>
2022-03-22 14:27:49 +0800

    some emit

---
+++ src/views/ComponentCommunicationChildRef.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style='border: 1px solid red; margin-left: 50px; width: 600px'>
+    本页面是child: <br/>
+    value_in_child: <input type="text" v-model="value_in_child"/>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value_in_child: ""
+    }
+  },
+}
+</script>
---
+++ src/views/ProvideLevelOne.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    我来自一级页面
+    <ProvideLevelTwo></ProvideLevelTwo>
+  </div>
+</template>
+
+<script>
+import ProvideLevelTwo from '@/components/ProvideLevelTwo'
+export default {
+  data () {
+    return {
+    }
+  },
+  components: {
+    ProvideLevelTwo
+  },
+  // 注意这个 provide()，所有在下层组件用到的prop，都可以在这里定义
+  provide() {
+    return {
+      readme_from_level_one: "我是在第一层页面被定义的, 在第三层应该被使用",
+    }
+  }
+}
+</script>
